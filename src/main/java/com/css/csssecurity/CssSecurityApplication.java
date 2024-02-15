972c0d266cd396d8b4f3c7f27f748293217c1677
toto <ahmadou@css.sn>
2024-02-15 12:02:26 +0000

    first commit

---
+++ src/main/java/com/css/csssecurity/CssSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.css.csssecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CssSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CssSecurityApplication.class, args);
+	}
+
+}
